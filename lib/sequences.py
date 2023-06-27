17b4c9afe6cb66d46874a0989518e0d791dc799b
danielpdaniel <artofjquinn@gmail.com>
2023-06-27 17:17:08 -0400

    Update print_fibonacci function to get tests to pass

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_nums = []
+    for n in range(length):
+        if n == 0:
+            fib_nums.append(0)
+        elif n == 1:
+            fib_nums.append(1)
+        else:
+            new_num = fib_nums[-1] + fib_nums[-2]
+            fib_nums.append(new_num)
+    
+    print(fib_nums)
